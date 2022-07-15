23db608048275e169e3d8a0fd4a150bd97b6ecf3
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-07-15 09:26:37 +0300

    commit tree 5ca12b8e98349fb009f17ccc1428746ea8454e72

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,76 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  // const [firstName, setFirstName] = useState("John");
+  // const [lastName, setLastName] = useState("Henry");
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  // function handleFirstNameChange(event) {
+  //   setFirstName(event.target.value);
+  // }
+
+  // function handleLastNameChange(event) {
+  //   setLastName(event.target.value);
+  // }
+
+  // refactoring
+  //============
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+    admin: false,
+  });
+
+  // function handleFirstNameChange(e) {
+  //   setFormData({ ...formData, firstName: e.target.value });
+  // }
+  // function handleLastNameChange(e) {
+  //   setFormData({ ...formData, lastName: e.target.value });
+  // }
+
+  // generic handleChange function
+  //==============================
+  function handleChange(e) {
+    const name = e.target.name; //name is KEY of formData obj we're updating
+    let value = e.target.value;
+
+    //use 'checked' property for checkboxes instead of 'value'
+    if (e.target.type === "checkbox") {
+      value = e.target.checked;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  // handleSubmit
+  //=============
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formData);
   }
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstName"
+        onChange={handleChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleChange}
+        value={formData.lastName}
+      />
+      <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
+      />
       <button type="submit">Submit</button>
     </form>
   );
